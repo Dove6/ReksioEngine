0ac18bcce9aab6c4d90df8b5df3a7f626d0fe1bb
Dawid <24943032+Dove6@users.noreply.github.com>
2026-07-10 16:18:12 +0200

    Implement KEYBOARD^GETLATESTKEY() and all key codes

--- src/engine/types/keyboard.ts
+++ src/engine/types/keyboard.ts
@@ -5,13 +5,85 @@ import { method } from '../../common/types'
 import { logger } from '../logging'
 
 const keysMapping = {
+    F1: 'F1',
+    F2: 'F2',
+    F3: 'F3',
+    F4: 'F4',
+    F5: 'F5',
+    F6: 'F6',
+    F7: 'F7',
+    F8: 'F8',
+    F9: 'F9',
+    F10: 'F10',
+    F11: 'F11',
+    F12: 'F12',
+    Escape: 'ESC',
+    Insert: 'INSERT',
+    Delete: 'DELETE',
+    PageUp: 'PGUP',
+    PageDown: 'PGDN',
+    Home: 'HOME',
+    End: 'END',
     ArrowLeft: 'LEFT',
-    ArrowRight: 'RIGHT',
     ArrowUp: 'UP',
     ArrowDown: 'DOWN',
+    ArrowRight: 'RIGHT',
+    CapsLock: 'CAPSLOCK',
+    ShiftLeft: 'LSHIFT',
+    ShiftRight: 'RSHIFT',
+    ControlLeft: 'LCTRL',
+    ControlRight: 'RCTRL',
+    AltLeft: 'LALT',
+    AltRight: 'RALT',
+    Enter: 'ENTER',
     Space: 'SPACE',
+    Tab: 'TAB',
+	KeyQ: 'Q',
+    KeyW: 'W',
+    KeyE: 'E',
+    KeyR: 'R',
+    KeyT: 'T',
+    KeyY: 'Y',
+    KeyU: 'U',
+    KeyI: 'I',
+    KeyO: 'O',
+    KeyP: 'P',
+    KeyA: 'A',
+    KeyS: 'S',
+    KeyD: 'D',
+    KeyF: 'F',
+    KeyG: 'G',
+    KeyH: 'H',
+    KeyJ: 'J',
+    KeyK: 'K',
+    KeyL: 'L',
+    KeyZ: 'Z',
+    KeyX: 'X',
+    KeyC: 'C',
+    KeyV: 'V',
+    KeyB: 'B',
+    KeyN: 'N',
+    KeyM: 'M',
     Digit1: '1',
     Digit2: '2',
+    Digit3: '3',
+    Digit4: '4',
+    Digit5: '5',
+    Digit6: '6',
+    Digit7: '7',
+    Digit8: '8',
+    Digit9: '9',
+    Digit0: '0',
+    Numpad1: '1',
+    Numpad2: '2',
+    Numpad3: '3',
+    Numpad4: '4',
+    Numpad5: '5',
+    Numpad6: '6',
+    Numpad7: '7',
+    Numpad8: '8',
+    Numpad9: '9',
+    Numpad0: '0'
 } as any
 
 type KeyState = { name: string; state: boolean }
@@ -21,11 +93,12 @@ export class Keyboard extends Type<KeyboardDefinition> {
     private changeQueue: KeyState[] = []
     private enabled: boolean = true
     private autoRepeat: boolean = false
+    private latestKey: string = ''
 
     private readonly onKeyDownCallback: (event: KeyboardEvent) => void
     private readonly onKeyUpCallback: (event: KeyboardEvent) => void
 
-    constructor(engine: Engine, parent: ParentType<any> | null, definition: MusicDefinition) {
+    constructor(engine: Engine, parent: ParentType<any> | null, definition: KeyboardDefinition) {
         super(engine, parent, definition)
         this.onKeyDownCallback = this.onKeyDown.bind(this)
         this.onKeyUpCallback = this.onKeyUp.bind(this)
@@ -71,6 +144,11 @@ export class Keyboard extends Type<KeyboardDefinition> {
     }
 
     @method()
+    GETLATESTKEY(): string {
+        return this.latestKey
+    }
+
+    @method()
     SETAUTOREPEAT(state: boolean) {
         this.autoRepeat = state
     }
@@ -97,7 +175,10 @@ export class Keyboard extends Type<KeyboardDefinition> {
             logger.warn(`Unsupported keyboard key code ${keyCode}`, {
                 supportedKeys: Object.keys(keysMapping),
             })
+            return
         }
+        if (value)
+            this.latestKey = mapped
         this.keysState.set(mapped, value)
         this.changeQueue.push({ name: mapped, state: value })
     }
