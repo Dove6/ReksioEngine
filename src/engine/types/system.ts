7f8fb30d88267756a12cd3ff88e3fdc5b6f986ef
Dawid <24943032+Dove6@users.noreply.github.com>
2026-07-13 02:13:27 +0200

    Implement SYSTEM^GETYEAR(), ^GETHOUR(), ^GETMINUTES(), ^GETSECONDS(), ^GETUSERNAME(), ^CREATEDIR()

--- src/engine/types/system.ts
+++ src/engine/types/system.ts
@@ -8,4 +8,39 @@ export class System extends Type<SystemDefinition> {
         const date = new Date()
         return date.getDate() - 0xf41db + (date.getMonth() + (date.getFullYear() - 1900) * 100) * 100
     }
+
+    @method()
+    GETYEAR(): number {
+        return new Date().getUTCFullYear()
+    }
+
+    @method()
+    GETHOUR(): number {
+        return new Date().getUTCHours()
+    }
+
+    @method()
+    GETMINUTES(): number {
+        return new Date().getUTCMinutes()
+    }
+
+    @method()
+    GETSECONDS(): number {
+        return new Date().getUTCSeconds()
+    }
+
+    @method()
+    async GETUSERNAME(): Promise<string> {
+        const hostname = location.hostname
+        if (/(^|\.)itch\.io/i.test(hostname)) {
+            return (await fetch('https://itch.io/api/1/me/me').then(r => r.json())).user.username
+        }
+        return 'Unknown'
+    }
+
+    @method()
+    async CREATEDIR(dirPath: string): Promise<boolean> {
+        await this.engine.filesystem.saveFile(dirPath.replace(/[^\/]+$/i, ''), new ArrayBuffer(0))
+        return true
+    }
 }
